74290bd4fe8cf30d321e4ade56d4763f419c7323
Martin Tajur <martin@tajur.ee>
2013-05-16 16:34:10 -0700

    new mysql, gitignore

--- index.js
+++ index.js
@@ -237,7 +237,7 @@ exports.Adapter = function(settings) {
 		return that;
 	};
 
-	this.comma_seperated_arguments = function(set) {
+	this.comma_separated_arguments = function(set) {
 		var clause = '';
 		if (Object.prototype.toString.call(set) === '[object Array]') {
 			clause = set.join(', ');
@@ -249,12 +249,12 @@ exports.Adapter = function(settings) {
 	};
 
 	this.group_by = function(set) {
-		groupByClause = this.comma_seperated_arguments(set);
+		groupByClause = this.comma_separated_arguments(set);
 		return that;
 	};
 
 	this.order_by = function(set) {
-		orderByClause = this.comma_seperated_arguments(set);
+		orderByClause = this.comma_separated_arguments(set);
 		return that;
 	};
 	
