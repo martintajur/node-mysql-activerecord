74001aef23c843577ebcf8532a70b3f523785cdf
Kyle Farris <kyle.farris@infotechfl.com>
2018-06-21 17:05:22 -0400

    Trying to make the full mock database creation script functional for Travis CI and non-Travis CI environments.

--- test/mysql/create_mysql_mock.sh
+++ test/mysql/create_mysql_mock.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-mysql -e 'create database mock_db;'
+mysql -uroot -e "CREATE DATABSE IF NOT EXISTS mock_db;"
+mysql -uroot -e "CREATE USER IF NOT EXISTS 'travis'@'localhost';"
+mysql -uroot -e "GRANT ALL ON `mock_db`.* TO 'travis'@'localhost';"
 mysql -utravis mock_db < ./test/mysql/mock_data.sql
