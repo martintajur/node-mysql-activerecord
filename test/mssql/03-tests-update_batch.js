b6190cdc4c2a9ac61d0bfc32431cfa5ac241550c
Kyle Farris <kyle.farris@infotechfl.com>
2018-07-26 10:32:55 -0400

    Added update batch test to mssql test suite.

--- test/mssql/03-tests-update_batch.js
+++ test/mssql/03-tests-update_batch.js
@@ -3,7 +3,7 @@ const expect = require('chai').expect;
 const QueryBuilder = require('../../drivers/mssql/query_builder.js');
 const qb = new QueryBuilder();
 
-const test_where = {id:3};
+const test_where = {quadrant: 'Alpha'};
 const test_data = [{id:3, name:'Milky Way', type: 'spiral'}, {id:4, name: 'Andromeda', type: 'spiral'}];
 
 describe('MSSQL: update_batch()', () => {
@@ -17,5 +17,10 @@ describe('MSSQL: update_batch()', () => {
         qb.reset_query();
         const sql = qb.update_batch('galaxies', test_data, 'id');
         sql.should.eql(["UPDATE ([galaxies]) SET [name] = CASE WHEN [id] = 3 THEN 'Milky Way' WHEN [id] = 4 THEN 'Andromeda' ELSE [name] END, [type] = CASE WHEN [id] = 3 THEN 'spiral' WHEN [id] = 4 THEN 'spiral' ELSE [type] END WHERE [id] IN (3,4)"]);
+    });
+    it('should build a proper batch UPDATE string when where clause is provided', () => {
+        qb.reset_query();
+        const sql = qb.update_batch('galaxies', test_data, 'id', test_where);
+        sql.should.eql(["UPDATE ([galaxies]) SET [name] = CASE WHEN [id] = 3 THEN 'Milky Way' WHEN [id] = 4 THEN 'Andromeda' ELSE [name] END, [type] = CASE WHEN [id] = 3 THEN 'spiral' WHEN [id] = 4 THEN 'spiral' ELSE [type] END WHERE [quadrant] = 'Alpha' AND [id] IN (3,4)"]);
     });
 });
