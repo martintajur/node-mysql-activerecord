6d27fb59a03e7b6de611d01c215dc1c347f51527
Kyle Farris <kyle.farris@infotechfl.com>
2019-10-28 18:05:12 -0400

    I feel like I'm actually getting closer...

--- test/mysql/create_mysql_mock.sh
+++ test/mysql/create_mysql_mock.sh
@@ -6,7 +6,7 @@ mysql -uroot -e "GRANT ALL ON mock_db.* TO 'travis'@'localhost';"
 mysql -uroot -e "GRANT ALL ON mock_db2.* TO 'travis'@'localhost';"
 mysql -uroot mock_db -e "DROP TABLE IF EXISTS cities;"
 mysql -uroot mock_db2 -e "DROP TABLE IF EXISTS cities2;"
-mysql -utravis mock_db < ./mock_data.sql
-mysql -utravis mock_db2 < ./mock_data2.sql
+mysql -utravis mock_db < ./test/mysql/mock_data.sql
+mysql -utravis mock_db2 < ./test/mysql/mock_data2.sql
 
 echo "Done with MySQL Import"
